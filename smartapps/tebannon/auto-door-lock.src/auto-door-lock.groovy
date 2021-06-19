9c8ff2a001b41754ffea710b07bd8f13767e67e6
Tom Bannon <tebannon@att.net>
2021-06-19 17:22:01 -0500

    Added oauth and pausible.

--- smartapps/tebannon/auto-door-lock.src/auto-door-lock.groovy
+++ smartapps/tebannon/auto-door-lock.src/auto-door-lock.groovy
@@ -19,12 +19,11 @@ definition(
     author: "Tom Bannon",
     description: "Automatically locks door after a specified elapsed time with optional door cotact sensor and motion contact sensor.",
     category: "Safety & Security",
+    singleInstance : false,
+    pausible: true,
+    auth: true,
 	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Solution/doors.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Solution/doors@2x.png"
-	/*
-	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png"
-    */
     )
 
 preferences
